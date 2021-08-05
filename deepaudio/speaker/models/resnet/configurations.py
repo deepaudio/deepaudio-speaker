71dc46b7ce05b6e6dd1a55f63b413a400f1e159a
yinruiqing <yinruiqing110@gmail.com>
2021-08-05 23:31:31 +0800

    feat: add model resnet101 from BUT vbx

---
+++ deepaudio/speaker/models/resnet/configurations.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, field
+
+from deepaudio.speaker.dataclass.configurations import DeepMMDataclass
+
+@dataclass
+class Resnet101Configs(DeepMMDataclass):
+    name: str = field(
+        default="resnet101", metadata={"help": "Model name"}
+    )
+    embed_dim: int = field(
+        default=256, metadata={"help": "Dimension of embedding."}
+    )
+    optimizer: str = field(
+        default="adam", metadata={"help": "Optimizer for training."}
+    )
+    min_num_frames: int = field(
+        default=200, metadata={"help": "Min num frames."}
+    )
+    max_num_frames: int = field(
+        default=400, metadata={"help": "Max num frames."}
+    )
+    squeeze_excitation: bool = field(
+        default=False, metadata={"help": "Max num frames."}
+    )
