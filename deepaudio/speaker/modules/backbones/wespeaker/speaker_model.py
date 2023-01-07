82243682ea0ea98a68172e147ef087dd175a4626
yinruiqing <yinruiqing110@gmail.com>
2023-01-07 18:42:05 +0800

    add: wespeaker models

---
+++ deepaudio/speaker/modules/backbones/wespeaker/speaker_model.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2022 Hongji Wang (jijijiang77@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import deepaudio.speaker.modules.backbones.wespeaker.tdnn as tdnn
+import deepaudio.speaker.modules.backbones.wespeaker.ecapa_tdnn as ecapa_tdnn
+import deepaudio.speaker.modules.backbones.wespeaker.resnet as resnet
+import deepaudio.speaker.modules.backbones.wespeaker.repvgg as repvgg
+
+
+def get_speaker_model(model_name: str):
+    if model_name.startswith("XVEC"):
+        return getattr(tdnn, model_name)
+    elif model_name.startswith("ECAPA_TDNN"):
+        return getattr(ecapa_tdnn, model_name)
+    elif model_name.startswith("ResNet"):
+        return getattr(resnet, model_name)
+    elif model_name.startswith("REPVGG"):
+        return getattr(repvgg, model_name)
+    else:  # model_name error !!!
+        print(model_name + " not found !!!")
+        exit(1)
+
+
+def MainModel(configs):
+    model_class = get_speaker_model(configs.model.name)
+    model = model_class(feat_dim=configs.feature.n_mels,
+                        embed_dim=configs.model.embed_dim,
+                        pooling_func=configs.model.pooling_func)
+    return model
